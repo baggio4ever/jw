392a3e1edb3e99f6ecb35cfaaf97ee8121cc1d68
baggio4ever <baggio4ever@gmail.com>
2018-06-06 12:17:41 +0900

    tslint の言うことを少し聞いた

--- src/app/jw-observatory/jw-observatory.component.ts
+++ src/app/jw-observatory/jw-observatory.component.ts
@@ -28,6 +28,13 @@ export class JwObservatoryComponent implements OnInit {
 
   @Output() loaded = new EventEmitter<Observatory>();
 
+  // https://qiita.com/tag1216/items/0b38ee5aedea0ef4a058
+  static getDEGfromDMM(v_deg, v_min): number {
+    const deg = v_deg + (v_min / 60.0);
+    return deg;
+  }
+
+
   constructor(private httpService: JwHttpService) { }
 
   ngOnInit() {
@@ -55,19 +62,20 @@ export class JwObservatoryComponent implements OnInit {
     return ret;
   }
 
-  // https://qiita.com/tag1216/items/0b38ee5aedea0ef4a058
-  static getDEGfromDMM(v_deg,v_min):number {
-    const deg = v_deg + (v_min / 60.0);
-    return deg;
-  }
-
   // http://www.shurey.com/html/googlemaps.html
   getGmapUrl(): string {
     if (this.observatory_info) {
       // 緯度
-      const latitude = JwObservatoryComponent.getDEGfromDMM(parseFloat(this.observatory_info.latitude),parseFloat(this.observatory_info.latitude_min));
+      const latitude = JwObservatoryComponent.getDEGfromDMM(
+                          parseFloat(this.observatory_info.latitude),
+                          parseFloat(this.observatory_info.latitude_min)
+                        );
+
       // 経度
-      const longitude = JwObservatoryComponent.getDEGfromDMM(parseFloat(this.observatory_info.longitude),parseFloat(this.observatory_info.longitude_min) );
+      const longitude = JwObservatoryComponent.getDEGfromDMM(
+                          parseFloat(this.observatory_info.longitude),
+                          parseFloat(this.observatory_info.longitude_min)
+                        );
 
       // return 'http://maps.google.co.jp/maps?ll=35.004212,135.869105';
       return 'http://maps.google.co.jp/maps?ll=' + latitude + ',' + longitude + '&z=16';
